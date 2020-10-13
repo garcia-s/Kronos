21d2f76fd1a056b09c15fe8619cc755dd7246834
garcia-s <43949658+garcia-s@users.noreply.github.com>
2020-10-13 07:59:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// $url = isset($_SERVER["PATH_INFO"]) ? explode('/', ltrim($_SERVER["PATH_INFO"], '/')) : [];
+
 require_once('./core/config.php');
 require_once('./helpers/functions.php');
 require_once('./core/core.php');
